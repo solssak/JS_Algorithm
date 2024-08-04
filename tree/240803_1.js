1f7404906b00712c186765c4a96ceb9d7aa41dec
solssak <gswoong1012@gmail.com>
2024-08-04 23:17:02 +0900

    tree

--- tree/240803_1.js
+++ tree/240803_1.js
@@ -19,3 +19,23 @@ function inorder(nodes, idx) {
 
   return "";
 }
+function postorder(nodes, idx) {
+  if (idx < nodes.length) {
+    let ret = postorder(nodes, idx * 2 + 1);
+    ret += postorder(nodes, idx * 2 + 2);
+    ret += `${nodes[idx]} `;
+    return ret;
+  }
+
+  return "";
+}
+
+function solution(nodes) {
+  return [
+    poreorder(nodes, 0).slice(0, -1),
+    inorder(nodes, 0).slice(0, -1),
+    postorder(nodes, 0).slice(0, -1),
+  ];
+}
+
+console.log(solution([1, 2, 3, 4, 5, 6, 7]));
